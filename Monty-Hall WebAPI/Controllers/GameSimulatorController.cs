69a4ff150eb7556a8b4d554fcb0342b31d750a41
razikusman <razikusman@gmail.com>
2023-08-06 00:36:43 +0530

    created Service class and moved simulation logic inside taht

--- Monty-Hall WebAPI/Controllers/GameSimulatorController.cs
+++ Monty-Hall WebAPI/Controllers/GameSimulatorController.cs
@@ -17,69 +17,16 @@ namespace Monty_Hall_WebAPI.Controllers
 
 
         [Microsoft.AspNetCore.Mvc.HttpPost]
-        public IActionResult SimulateGames(double simulations = 0, bool switchorNot = false)
+        public IActionResult SimulateGames(long simulations = 0, bool switchorNot = false)
         {
 
             //random number generator
 
-            MontyHallSimulations simulation = new MontyHallSimulations();
-
-            simulation.NumberOfSimulations = simulations.ToString(); //number of simulation
-            simulation.SwitchDoor = switchorNot.ToString(); // switching status 
-
-            double winCount = 0; // initila wincount with 0
-
-            for (int i = 0; i < simulations; i++)
-            {
-
-                //select Random winning Door
-                var _winDoor = doorSelection();
-
-                // select Random Player Choice Door
-                var _playerChoiceDoor = doorSelection();
-
-                //host opening door in Random
-                var _hostOpeningDoor = doorSelection();
-
-                //the door shouldnt be of winning or playerchoice
-                while (_hostOpeningDoor == _winDoor || _hostOpeningDoor == _playerChoiceDoor)
-                {
-                    _hostOpeningDoor = doorSelection();
-                }
-
-                //if player choos to switch the door
-                if (switchorNot == true)
-                {
-                    var previousechoice = _playerChoiceDoor;
-
-                    //select a new door
-                    _playerChoiceDoor = doorSelection();
-
-                    //if previous and current are same create a new or its host selected one
-                    while (_playerChoiceDoor == previousechoice || _playerChoiceDoor == _hostOpeningDoor)
-                    {
-                        _playerChoiceDoor = doorSelection();
-                    }
-                }
-
-                //check wining or not
-                winCount += _playerChoiceDoor == _winDoor ? 1 : 0;
-
-            }
-
-            //at the ENd of all simulation calculte percentage of win
-            simulation.WinPercent = (winCount / simulations) * 100;
+            GameService gameService = new GameService();
+            var simulationResult = gameService.getResult(simulations, switchorNot);
 
 
-            return ToJson(simulation);
-        }
-
-        private int doorSelection()
-        {
-            //random number generator
-            var _randomDoorGenerator = new Random();
-
-            return _randomDoorGenerator.Next(1, 4);
+            return ToJson(simulationResult);
         }
     }
 }
